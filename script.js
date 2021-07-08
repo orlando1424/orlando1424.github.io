ee11ce588d2ae37892da2353eb0bcde90cdfaf9a
Orlando <lopezorlando23@gmail.com>
2021-07-08 13:59:03 -0600

    personalize theme

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("Its working");
+
+let themeDots = document.getElementsByClassName("theme-dot");
+
+for (var i = 0; themeDots.length > i; i++) {
+  themeDots[i].addEventListener("click", function () {
+    let mode = this.dataset.mode;
+    console.log("Option clicked:", mode);
+    setTheme(mode);
+  });
+}
+
+function setTheme(mode) {
+  if (mode == "light") {
+    document.getElementById(("theme-style".href = "/css/style.css"));
+  }
+
+  if (mode == "blue") {
+    document.getElementById(("theme-style".href = "blue.css"));
+  }
+
+  if (mode == "green") {
+    document.getElementById(("theme-style".href = "/css/green.css"));
+  }
+
+  if (mode == "purple") {
+    document.getElementById(("theme-style".href = "/css/purple.css"));
+  }
+}
